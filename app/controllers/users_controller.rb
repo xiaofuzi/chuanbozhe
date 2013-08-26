60d11964227ca68e7ed839fb8a19414978357c67
fengxue <fengxue@fengxue-Aspire-5560.(none)>
2013-08-26 13:19:14 +0800

    改进首先显示问题

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:index, :edit, :show, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
       sign_in @user
-  		flash[:success] = "Welcome to the Sample App!"
+  		flash[:success] = "欢迎来到传播者!"
   		redirect_to @user
   	else
   		render 'new'
